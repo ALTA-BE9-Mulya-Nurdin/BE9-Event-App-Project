f84eb1d20e5102bb9e6aa186a22fa7344bda4b78
Mulya Nurdin <mulyanurdin10@gmail.com>
2022-07-07 13:25:36 +0700

    changes to update user

--- features/users/business/usecase.go
+++ features/users/business/usecase.go
@@ -27,9 +27,8 @@ func (usecase *userUsecase) InsertData(insert users.Core) (row int, err error) {
 	return row, err
 }
 
-func (usecase *userUsecase) GetAllData(limit, offset int) (data []users.Core, err error) {
-	var param string
-	data, err = usecase.userData.GetAllData(param)
+func (usecase *userUsecase) GetAllData() (data []users.Core, err error) {
+	data, err = usecase.userData.GetAllData()
 	return data, err
 }
 
--- features/users/presentation/handler.go
+++ features/users/presentation/handler.go
@@ -8,7 +8,6 @@ import (
 	_middlewares "be9/event/middlewares"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -53,11 +52,7 @@ func (h *UserHandler) InsertData(c echo.Context) error {
 }
 
 func (h *UserHandler) GetAllData(c echo.Context) error {
-	limit := c.QueryParam("limit")
-	offset := c.QueryParam("offset")
-	limitint, _ := strconv.Atoi(limit)
-	offsetint, _ := strconv.Atoi(offset)
-	data, err := h.userBusiness.GetAllData(limitint, offsetint)
+	data, err := h.userBusiness.GetAllData()
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("failed to get all data"))
 	}
